10e54ad7b93a0d97599a9744ac93f4a3ab3c8b9d
KevinGastelum <kev.gas777@gmail.com>
2024-03-19 14:46:40 -0700

    Lambda Function created

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,8 @@
+import json
+
 def handler(event, context):
-  response_body = {
-    'message': 'Hello World',
-    'version': '1.0.0'
-  }
-  return {'statusCode': 200, 'body': response_body}
+    response_body = {
+        'message': 'Hello World',
+        'version': '1.0.0'
+    }
+    return {'statusCode': 200, 'body': json.dumps(response_body)}
