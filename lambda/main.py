3198dd9f5cbdb74507a489fd90ff515e33d7baa2
KevinGastelum <kev.gas777@gmail.com>
2024-03-19 17:41:47 -0700

    Lambda Function created

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def handler(event, context):
-    version = os.environ.get=('VERSION', '0.0')
+    version = os.environ.get('VERSION', '0.0')
     response_body = {'message': 'Hello World', 'version': version}
     return {"statusCode": 200, "body": response_body}
+
+# print(os.environ.get("VERSION"))
